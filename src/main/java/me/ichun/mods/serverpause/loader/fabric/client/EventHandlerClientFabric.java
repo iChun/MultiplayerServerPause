600201b8ea2c97a1015e7de00ca021e6be726b89
iChun <ohai.iChun@gmail.com>
2023-02-04 20:01:56 +0000

    Update Specification-Version in Manifest. Use the right Fabric Event. I can't believe no one realised this was broken before this.

--- src/main/java/me/ichun/mods/serverpause/loader/fabric/client/EventHandlerClientFabric.java
+++ src/main/java/me/ichun/mods/serverpause/loader/fabric/client/EventHandlerClientFabric.java
@@ -3,6 +3,7 @@ package me.ichun.mods.serverpause.loader.fabric.client;
 import me.ichun.mods.serverpause.client.core.EventHandlerClient;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.networking.v1.ClientLoginConnectionEvents;
+import net.fabricmc.fabric.api.client.networking.v1.ClientPlayConnectionEvents;
 
 public class EventHandlerClientFabric extends EventHandlerClient
 {
@@ -11,6 +12,6 @@ public class EventHandlerClientFabric extends EventHandlerClient
         ClientTickEvents.END_CLIENT_TICK.register(client -> onClientTickEnd());
 
         ClientLoginConnectionEvents.INIT.register((handler, client) -> onClientServerConnectionChange());
-        ClientLoginConnectionEvents.DISCONNECT.register((handler, client) -> onClientServerConnectionChange());
+        ClientPlayConnectionEvents.DISCONNECT.register((handler, client) -> onClientServerConnectionChange());
     }
 }
